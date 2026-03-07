a23323677992520d16cc508c34122bf73544e925
Kirtos <kirtgamer84@gmail.com>
2026-03-07 01:07:33 -0500

    def index

--- app.py
+++ app.py
@@ -200,10 +200,6 @@ def dashboard():
         total_gasto=total_gasto
     )
 
-@app.route("/")
-def home():
-    return "Moto Control funcionando 🚀"
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
